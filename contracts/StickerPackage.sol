8e18966d06632eb6a8ee67b10642c19b4ce873aa
Borja Garibotti <mgaribotti@fi.uba.ar>
2022-11-12 17:30:24 -0300

    :construction: Update StickerPackage.sol

--- contracts/StickerPackage.sol
+++ contracts/StickerPackage.sol
@@ -1,42 +1,73 @@
-//SPDX-License-Identifier: MIT
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.17;
 
-contract QatarStickerPackageContract {
-    struct StickerPackage {
-        uint256 id;
-        bool opened;
+// import Ownable
+// import SafeMath
+
+interface Coin {
+  function consume(uint _amount);
+}
+
+interface Sticker {
+  function createSticker(address _address, uint _amount);
+}
+
+contract StickerPackageContract {
+  using SafeMath for uint;
+
+  address owner;
+  address coinAddress;
+  address stickerAddress;
+  uint price;
+  uint stickersPerPackages = 5;
+  
+  mapping(address => uint) packagesFromUser;
+
+  constructor() {
+    owner = msg.sender;
+  }
+
+  modifier onlyOwner() {
+    require(owner == msg.sender);
+    _;
+  }
+
+  modifier withPriceSetted() {
+    require(price > 0);
+    _;
+  }
+
+  function setStickersPerPackage(uint _amount) external onlyOwner {
+    require(_amount > 0);
+    stickersPerPackages = _amount;
+  }
+
+  function setPrice(uint _price) external onlyOwner {
+    price = _price; 
+  }
+  
+  function setCoinAddress(address _coinAddress) external onlyOwner {
+    coinAddress = _coinAddress;
+  }
+
+  function setStickerAddress(address _stickerAddress) external onlyOwner {
+    stickerAddress = _stickerAddress;
+  }
+
+  function buyPackages(uint _quantity) external withPriceSetted {
+    Coin coinContact = Coint(coinAddress).consume(_quantity * price);
+    for(uint i; i < _quantity; i++) {
+      packagesFromUser[msg.sender] = packagesFromUser[msg.sender].add(1);
     }
+  }
 
-    uint256 totalPackages;
-    mapping(address => StickerPackage[]) walletToPackages;
+  function openPackage() external {
+    require(packagesFromUser[msg.sender] > 0);
+    Sticker stickerContract = Sticker(stickerAddress).createStickers(msg.sender, stickersPerPackages);
+    packagesFromUser[msg.sender] = packagesFromUser[msg.sender].sub(1);
+  }
 
-    function buyPackage() public {
-        uint256 packageId = totalPackages++;
-        StickerPackage[] storage walletPackages = walletToPackages[msg.sender];
-        StickerPackage memory newPackage = StickerPackage({
-            id: packageId,
-            opened: false
-        });
-        walletPackages.push(newPackage);
-    }
-
-    function listPackages() public view returns (StickerPackage[] memory) {
-        StickerPackage[] memory walletPackages = walletToPackages[msg.sender];
-        return walletPackages;
-    }
-
-    function openPackage(uint256 _packageId) public {
-        StickerPackage[] storage walletPackages = walletToPackages[msg.sender];
-        for (uint256 i = 0; i < walletPackages.length; i++) {
-            StickerPackage storage package = walletPackages[i];
-            if (package.id != _packageId) continue;
-
-            _openPackage(package);
-            break;
-        }
-    }
-
-    function _openPackage(StickerPackage storage package) private {
-        package.opened = true;
-    }
+  function getAmountOfPackagesFrom(address _owner) external view returns (uint) {
+    return packagesFromUser[_owner];
+  }
 }
