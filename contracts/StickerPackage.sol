352007748b74e22c851162e742d27c0692fff33d
Borja Garibotti <mgaribotti@fi.uba.ar>
2022-11-13 21:10:56 -0300

    :construction: Update StickerPackage.sol

--- contracts/StickerPackage.sol
+++ contracts/StickerPackage.sol
@@ -1,36 +1,26 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.17;
 
-// import Ownable
-// import SafeMath
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 
-interface Coin {
-  function consume(uint _amount);
+interface FiubaCoinInterface {
+  function transfermFrom(address _from, address _to, uint _amount);
 }
 
-interface Sticker {
-  function createSticker(address _address, uint _amount);
+interface StickerInterface {
+  function createStickers(address _address, uint _amount);
 }
 
-contract StickerPackageContract {
+contract StickerPackageContract is Ownable {
   using SafeMath for uint;
 
-  address owner;
   address coinAddress;
   address stickerAddress;
   uint price;
   uint stickersPerPackages = 5;
   
   mapping(address => uint) packagesFromUser;
-
-  constructor() {
-    owner = msg.sender;
-  }
-
-  modifier onlyOwner() {
-    require(owner == msg.sender);
-    _;
-  }
 
   modifier withPriceSetted() {
     require(price > 0);
@@ -46,7 +36,7 @@ contract StickerPackageContract {
     price = _price; 
   }
   
-  function setCoinAddress(address _coinAddress) external onlyOwner {
+  function setFiubaCoinAddress(address _coinAddress) external onlyOwner {
     coinAddress = _coinAddress;
   }
 
@@ -55,15 +45,17 @@ contract StickerPackageContract {
   }
 
   function buyPackages(uint _quantity) external withPriceSetted {
-    Coin coinContact = Coint(coinAddress).consume(_quantity * price);
-    for(uint i; i < _quantity; i++) {
-      packagesFromUser[msg.sender] = packagesFromUser[msg.sender].add(1);
-    }
+    FiubaCoinInterface coinContact = FiubaCoinInterface(coinAddress).transferFrom(
+      msg.sender,
+      address(this),
+      _quantity * price
+    );
+    packagesFromUser[msg.sender] = packagesFromUser[msg.sender].add(_quantity);
   }
 
   function openPackage() external {
     require(packagesFromUser[msg.sender] > 0);
-    Sticker stickerContract = Sticker(stickerAddress).createStickers(msg.sender, stickersPerPackages);
+    StickerInterface stickerContract = StickerInterface(stickerAddress).createStickers(msg.sender, stickersPerPackages);
     packagesFromUser[msg.sender] = packagesFromUser[msg.sender].sub(1);
   }
 
