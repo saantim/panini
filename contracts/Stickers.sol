fae97112e19ac86726619cf8c1f1a2ddcf735f8c
ovr4ulin <t.baldi.ma@gmail.com>
2022-11-20 16:44:51 -0300

    reordena el codigo

--- contracts/Stickers.sol
+++ contracts/Stickers.sol
@@ -2,19 +2,51 @@
 pragma solidity ^0.8.17;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract QatarSticker is ERC721 {
+contract QatarSticker is ERC721, Ownable{
+    
+    // ---------------------------------------------------
+    // public variables
+    // ---------------------------------------------------
     uint256 public totalMints = 0;
-    uint256 private maxPlayer = 711;
-    address owner;
     uint256 public mintPrice = 1000000 gwei; 
     string public URI = "https://bafybeifqmgyfy4by3gpms5sdv3ft3knccmjsqxfqquuxemohtwfm7y7nwa.ipfs.dweb.link/metadata.json";
-
+    
     mapping(uint256 => uint256) public stickerToPlayer;
     mapping(uint256 => string) public playerToURI; 
+    
+    // ---------------------------------------------------
+    // private variables
+    // ---------------------------------------------------
+    address private packageContractAddress;
+    uint256 private maxPlayer = 711;
 
+    // ---------------------------------------------------
+    // configuration
+    // ---------------------------------------------------
+    function setPackageContractAddress(address newPackageContractAddress) public onlyOwner {
+        packageContractAddress = newPackageContractAddress;
+    }
+
+    // ---------------------------------------------------
+    // modifiers
+    // ---------------------------------------------------
+    modifier onlyPackageContract(){
+        require(msg.sender == packageContractAddress);
+        _;
+    }
+
+    // ---------------------------------------------------
+    // functions
+    // ---------------------------------------------------
     constructor() ERC721("QatarSticker", "QST") {
-        owner = msg.sender;
+    }
+
+    function createStickers(address packageOwner, uint256 quantity) public onlyPackageContract {
+        for (uint i = 0; i < quantity; i++) {
+            safeMint(packageOwner);
+        }
     }
 
     function safeMint(address to) internal {
@@ -23,6 +55,15 @@ contract QatarSticker is ERC721 {
         totalMints++;
 
         _safeMint(to, tokenId);
+    }
+
+    function randomPlayerId() private view returns (uint256) {
+        uint randId = uint(keccak256(abi.encodePacked(totalMints)));
+        return randId % maxPlayer; 
+    }
+
+    function getPlayerIdFromStickerId(uint256 stickerId) public view returns (uint256) {
+        return stickerToPlayer[stickerId];
     }
 
     function getStickersFromWallet(address wallet) public view returns (uint256[] memory) {
@@ -37,26 +78,5 @@ contract QatarSticker is ERC721 {
         }
 
         return result;
-    }
-
-    function getPlayerIdFromStickerId(uint256 stickerId) public view returns (uint256) {
-        return stickerToPlayer[stickerId];
-    }
-
-    function mintToken(uint256 quantity_) public payable {
-        require(quantity_ * mintPrice == msg.value, "wrong amount sent");
-
-        for (uint i; i < quantity_; i++) {
-            safeMint(msg.sender);
-        }
-    }
-
-    function randomPlayerId() private view returns (uint256) {
-        uint randId = uint(keccak256(abi.encodePacked(block.timestamp)));
-        return randId % maxPlayer; 
-    }
-
-    function withdraw() public {
-        payable(owner).transfer(address(this).balance);
     }
 }
